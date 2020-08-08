f5d779b46454b3b11c76c34be214cfbda693a5a9
levantAJ <sirlevantai@gmail.com>
2020-08-08 10:42:15 +0700

    Initial

---
+++ ShapeDrawer/ShapeDrawer.h
@@ -0,0 +1,18 @@
+//
+//  ShapeDrawer.h
+//  ShapeDrawer
+//
+//  Created by Tai Le on 8/8/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShapeDrawer.
+FOUNDATION_EXPORT double ShapeDrawerVersionNumber;
+
+//! Project version string for ShapeDrawer.
+FOUNDATION_EXPORT const unsigned char ShapeDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShapeDrawer/PublicHeader.h>
+
+
